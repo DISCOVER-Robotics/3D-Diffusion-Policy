bd7d9272c276df8481587f0e5ed4d81a07c95210
Rainerino <albertyanyy@gmail.com>
2026-02-17 14:31:24 +0800

    update

--- 3D-Diffusion-Policy/eval.py
+++ 3D-Diffusion-Policy/eval.py
@@ -1,19 +1,10 @@
-if __name__ == "__main__":
-    import sys
-    import os
-    import pathlib
-
-    ROOT_DIR = str(pathlib.Path(__file__).parent.parent.parent)
-    sys.path.append(ROOT_DIR)
-    os.chdir(ROOT_DIR)
-
 import os
 import hydra
 import torch
 import dill
 from omegaconf import OmegaConf
 import pathlib
-from train import TrainDP3Workspace
+from diffusion_policy_3d.workspace import TrainDP3Workspace
 
 OmegaConf.register_new_resolver("eval", eval, replace=True)
     
