01e78dfafc511350270f85d8bc5fc939d20d8e3a
Rainerino <albertyanyy@gmail.com>
2026-02-17 19:13:47 +0800

    update

--- 3D-Diffusion-Policy/train.py
+++ 3D-Diffusion-Policy/train.py
@@ -1,20 +1,6 @@
-import os
 import hydra
-import torch
-import dill
 from omegaconf import OmegaConf
 import pathlib
-from torch.utils.data import DataLoader
-import copy
-import random
-import wandb
-import tqdm
-import numpy as np
-from termcolor import cprint
-import shutil
-import time
-import threading
-from hydra.core.hydra_config import HydraConfig
 from diffusion_policy_3d.workspace import TrainDP3Workspace
 
 OmegaConf.register_new_resolver("eval", eval, replace=True)
