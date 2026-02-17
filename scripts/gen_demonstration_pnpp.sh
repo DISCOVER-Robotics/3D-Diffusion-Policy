f4ff5d029c55fda1c46dab9dfc4fb87be48823a7
Rainerino <albertyanyy@gmail.com>
2026-02-17 13:36:17 +0800

    update

--- scripts/gen_demonstration_pnpp.sh
+++ scripts/gen_demonstration_pnpp.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPO_ROOT="$(cd "${SCRIPT_DIR}/../../../../" && pwd)"
 
 num_episodes="${1:-10}"
-root_dir="${REPO_ROOT}/compute/models/dp3/3D-Diffusion-Policy/data/"
+root_dir="${REPO_ROOT}/compute/src/compute/models/dp3/3D-Diffusion-Policy/data/"
 cam_id="${CAM_ID:-cam1}"
 
 uv run --project "${REPO_ROOT}/compute" \
